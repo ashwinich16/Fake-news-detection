90a19a04dcff36bcb2347823f68a2fe21544ce0c
ashwinich16 <54015735+ashwinich16@users.noreply.github.com>
2021-08-10 23:08:04 +0530

    Update fake news detection.py

--- fake news detection.py
+++ fake news detection.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
 
 
 import numpy as np
@@ -13,46 +12,35 @@ from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.metrics import accuracy_score,confusion_matrix
 
 
-# In[2]:
-
 
 df = pd.read_csv('news.csv')
 df.head()
 
 
-# In[3]:
 
 
 labels = df.label
 labels.head()
 
 
-# In[4]:
-
-
 x_test,x_train,y_test,y_train = train_test_split(df['text'],labels,test_size=0.2,random_state=7)
 
-
-# In[5]:
 
 
 #intializing TfidfVectorizer
 Tfidf_Vectorizer = TfidfVectorizer(stop_words ='english',max_df=0.7)
 
-#fit and transform train set
+
 tfidf_train = Tfidf_Vectorizer.fit_transform(x_train)
 tfidf_test = Tfidf_Vectorizer.transform(x_test)
 
 
-# In[6]:
 
 
 #intializing PassiveAggressiveClassifier
 pac = PassiveAggressiveClassifier(max_iter=50)
 pac.fit(tfidf_train,y_train)
 
-
-# In[7]:
 
 
 #prediction on test set and calculating accuracy
